86c0466012d75964844c88d7234a44b973575241
kiyota.takashi <kiyota.takashi@classmethod.jp>
2020-08-14 00:47:03 +0900

    :recycle: modify material-ui setup

--- packages/client/src/pages/_document.tsx
+++ packages/client/src/pages/_document.tsx
@@ -5,37 +5,29 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document'
-import { ServerStyleSheet as StyledComponentSheets } from 'styled-components'
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/styles'
 
 export default class extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const styledComponentSheets = new StyledComponentSheets()
     const materialUiServerStyleSheets = new MaterialUiServerStyleSheets()
     const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            styledComponentSheets.collectStyles(
-              materialUiServerStyleSheets.collect(<App {...props} />)
-            ),
-        })
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          materialUiServerStyleSheets.collect(<App {...props} />),
+      })
 
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {styledComponentSheets.getStyleElement()}
-            {materialUiServerStyleSheets.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      styledComponentSheets.seal()
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {materialUiServerStyleSheets.getStyleElement()}
+        </>
+      ),
     }
   }
 
