04c3f649c7bdb2df6e81c962bec94167d2bd9141
kiyota.takashi <kiyota.takashi@classmethod.jp>
2020-08-13 22:43:22 +0900

    :rotating_light: modify lint config

--- packages/client/src/pages/_app.tsx
+++ packages/client/src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-// FIXME:
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
